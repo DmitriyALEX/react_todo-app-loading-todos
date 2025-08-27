815dda9653ec9400fa91c41544c1c327e311bef3
DmitriyALEX <doiroffical@gmail.com>
2025-08-27 22:48:20 +0300

    add task solution

---
+++ src/components/errorNotifacations.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import cn from 'classnames';
+
+type Props = {
+  isError: string;
+};
+
+const ErrorNotifacations: React.FC<Props> = ({ isError }) => {
+  const [isCloseNotification, setIsCloseNotification] =
+    useState<boolean>(false);
+
+  return (
+    // {/* DON'T use conditional rendering to hide the notification */}
+    // {/* Add the 'hidden' class to hide the message smoothly */}
+    <div
+      data-cy="ErrorNotification"
+      className={cn('notification is-danger is-light has-text-weight-normal', {
+        hidden: !isError || isCloseNotification,
+      })}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => setIsCloseNotification(true)}
+      />
+      {/* show only one message at a time */}
+
+      {isError}
+      {/* Unable to load todos
+      <br />
+      Title should not be empty
+      <br />
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo */}
+    </div>
+  );
+};
+
+export default ErrorNotifacations;
